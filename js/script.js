9704e949091b9690455914b52cdf56849d382fed
Stas <info7762@gmail.com>
2019-10-13 15:13:07 +0600

    Modal window

--- js/script.js
+++ js/script.js
@@ -90,13 +90,26 @@ window.addEventListener('DOMContentLoaded', function() {
                 minutes.textContent = '00';
                 seconds.textContent = '00';
             }
-           
         }
-
     }
-
     setClock('timer', deadline);
 
+    //Modal
 
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document. querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
 
 });
